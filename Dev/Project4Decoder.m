d7fa9a0dfaacd924debc60abc99432ae1dff7019
LegallyKF5RCL <kf5rcl@gmail.com>
2013-12-02 01:48:05 -0600

    Cleaned Decoder a little

--- Dev/Project4Decoder.m
+++ Dev/Project4Decoder.m
@@ -12,29 +12,16 @@ Data = Signal;
 
 
 Samples = 8000;              %sampling frequency 
-Time = .02;                   %time in seconds alloted for each bit
-SampTime = Samples*Time;        %ratio of samples to 1 second
+Time = .02;                  %time in seconds alloted for each bit
+SampTime = Samples*Time;     %ratio of samples to 1 second
 TotalLen = length(Data);
-Fqs = [697 770 852 941 1209 1336 1477 1633];
+Fqs = [697 770 852 941 1209 1336 1477 1633];    %the frequencies
 FreqIndecies = round(Fqs / Samples * SampTime + 1);
 
 TToneTable = ['1', '2', '3', 'A';
                 '4', '5', '6', 'B';
                 '7', '8', '9', 'C';
                 'E', '0', 'F', 'D'];
-
-%Data = Encodedmessage;
-
-%CharLen = 2;                            %length of char will always be 7
-%StringLen = TotalLen/CharLen;
-
-
-%make matricies...
-%AxisX = linspace(-Fs/2, Fs/2, Fs);              %variable for plotting the frequency domain
-%X = linspace(0,BitLength,Fs);                   %create time domain
-%Y = zeros([TotalLen,Fs]);                          %create a zero matrix for the signals
-%DecodedMessage = zeros([TotalLen,Fs]);              %create a zero matrix for decoding the message
-%DecodedBinary = int8(zeros([StringLen,CharLen]));   %create a zero matrix for the bits after the message is decoded
 
 PositionCounter = 0;
 IterationCounter = 1;
@@ -44,18 +31,18 @@ RowCounter = 1;
 
 %find when the signal starts
 for i = 1:TotalLen
-    if Data(1, i) > 1.6
-        PositionCounter = i;
-        DelayPoints = i;
-        break
+    if Data(1, i) > 1.6     %when the signal starts
+        %PositionCounter = i;    
+        DelayPoints = i;    %record the position we are in
+        break               %leave loop
     end
 end
 
 %make data readable
 for ii = 1:(TotalLen - DelayPoints)
-    
+    %take away the delay
     OrganizedData(RowCounter, ArbCounter) = Data(1, ii + DelayPoints);
-    ArbCounter = ArbCounter + 1;
+    ArbCounter = ArbCounter + 1;    %begin counting
     if mod(ArbCounter - 1, 160) == 0
         ArbCounter = 1;
         RowCounter = RowCounter + 1;
@@ -94,17 +81,4 @@ disp('~~~~~~~');
 disp(Print');
 disp('~~~~~~~');
 
-
-%{
-for kk = 1:StringLen                        %loop through everything
-    for asdf = 1:CharLen
-        B(kk,asdf) = dec2bin(A(kk,asdf));   %convert the decimals to a binary sequence
-    end
-end
-B = hex2dec(B);     %convert the binary sequence into a decimal number that represents the message character
-
-disp('~~~~~');
-disp(char(B'));
-disp('~~~~~');
-%}
 disp('Finished...');
